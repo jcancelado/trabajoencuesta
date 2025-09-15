5aaa0f4343f74ae6936e2d44a01aed9a91f0c32d
JhosepCastro <144240720+JhosepCastro@users.noreply.github.com>
2025-09-14 21:53:59 -0500

    xxx

--- ejerciciocreado.py
+++ ejerciciocreado.py
@@ -1,2 +1,15 @@
 class Libro:
+    def __init__(self, titulo, auto, año):
+        self.titulo
+        self.auto
+        self.año
+
+class Biblioteca:
+    def __init__(self):
+        self.libros = []
+    def guardar_libro():
+        libro = input("Ingrese un libro")
+
+
+
     print("")
